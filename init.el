95ccfd0938876af94c0233b0a5af4a12b641f961
MattLu <mattlu@MattLu.lan>
2018-02-14 21:37:54 +0800

    ignore unnessary file

---
+++ init.el
@@ -0,0 +1,10 @@
+(package-initialize)
+
+;; I use =cask= and =pallet= for managing packages. These need to be included in
+;; here to ensure that the correct version of =org= is used to render my
+;; =configuration.org= file.
+(require 'cask "/usr/local/share/emacs/site-lisp/cask/cask.el")
+(cask-initialize)
+(require 'pallet)
+
+(org-babel-load-file "~/.emacs.d/configuration.org")
