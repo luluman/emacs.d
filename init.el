e1be9d872b4074504e4b39d180d2bdd5aa8d8282
man.lu <man.lu@bitmain.com>
2019-11-08 02:08:27 +0800

    add company statistics

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 (require 'package)
 (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
 (package-initialize)
 
 ;; Ensure that use-package is installed.
