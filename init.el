7d3d71575678721ecdc2afdebcca303db1dc52bf
MattLu <lu.man@outlook.com>
2020-05-22 20:05:34 +0800

    enhance use-package using quelpa, which support Github url

--- init.el
+++ init.el
@@ -2,6 +2,7 @@
 (setq package-archives
       '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
         ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
+        ("melpa-stable" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa-stable/")
         ("org" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/")
         ))
 (package-initialize)
